cb77439fbd74ae24b1be5494b3b054831013da35
anton1661sam <90688741+anton1661sam@users.noreply.github.com>
2024-01-19 15:31:01 +0300

    Add files via upload

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,31 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Главная</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Панель</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Марки</a>
+<ul>
+<li><a href="create-brand.php">Добавить марку</a></li>
+<li><a href="manage-brands.php">Редактировать</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Авто</a>
+					<ul>
+						<li><a href="post-avehical.php">Добавить авто</a></li>
+						<li><a href="manage-vehicles.php">Редактировать</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Править аренды</a></li>
+
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Править отзывы</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop""></i> Manage Conatctus Query</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Добавить пользователя</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Править пользователей</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Обновить контактную информацию</a></li>
+
+			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Править подписчиков</a></li>
+
+			</ul>
+		</nav>
